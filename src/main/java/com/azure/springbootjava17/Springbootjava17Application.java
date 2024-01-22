d65102eff27b9967f8854ba2724a6f294c71ae95
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2024-01-22 16:13:43 -0300

    feat: adiciona jar

--- src/main/java/com/azure/springbootjava17/Springbootjava17Application.java
+++ src/main/java/com/azure/springbootjava17/Springbootjava17Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Springbootjava17Application extends SpringBootServletInitializer {
+public class Springbootjava17Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springbootjava17Application.class, args);
