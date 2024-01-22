ea4a927267aa280ed995d3baa372c4f9bbc85b45
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2024-01-22 14:49:34 -0300

    feat: add controlador rest

---
+++ src/main/java/com/azure/springbootjava17/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package com.azure.springbootjava17.controller;
+
+import com.azure.springbootjava17.entity.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @GetMapping
+    public Customer customer(){
+        return new Customer(1L, "Jefferson Luiz", "Welcome Jefferson!");
+    }
+}
