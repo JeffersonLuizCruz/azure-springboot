c91f0c47be0607c49f86b002dd9b7ff84e3e18b3
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2024-01-22 15:30:42 -0300

    feat: adiciona gerador war

--- src/main/java/com/azure/springbootjava17/Springbootjava17Application.java
+++ src/main/java/com/azure/springbootjava17/Springbootjava17Application.java
@@ -2,9 +2,10 @@ package com.azure.springbootjava17;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Springbootjava17Application {
+public class Springbootjava17Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springbootjava17Application.class, args);
